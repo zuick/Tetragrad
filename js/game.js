5964fa441c0b588d9691d85fe9efda606cdde8d0
ruslan.khakimov <ruslan.khakimov@frumatic.com>
2014-03-02 23:29:47 +0600

    falling shape tiles

--- js/game.js
+++ js/game.js
@@ -9,7 +9,8 @@
         antialias: false,
         
         levels: [ 'level1', 'level2' ],
-        collidableTiles: [ 0, 4, 5, 6, 7, 8, 9, 10, 11 ]
+        collidableTiles: [ 0, 4, 5, 6, 7, 8, 9, 10, 11 ],
+        fallingShapesTiles: [ 8, 9, 10, 11 ]
     }
     
     var game = new Phaser.Game( config );
